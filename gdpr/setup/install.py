c08882aefbe74aab043062619a95cb90a16bc187
Raffael Meyer <raffael@alyf.de>
2019-06-04 15:55:58 +0200

    better variable name

--- gdpr/setup/install.py
+++ gdpr/setup/install.py
@@ -3,6 +3,6 @@ import frappe
 from default_documents import get_data
 
 def after_install():
-    data = get_data()
-    for doc in data:
+    default_documents = get_data()
+    for doc in default_documents:
         frappe.get_doc(doc).insert()
