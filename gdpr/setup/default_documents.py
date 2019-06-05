c44a5de7d9650bae42fe9622a1595436f0f765ba
Paul Pereira <paul.pereira@alyf.de>
2019-06-05 16:35:16 +0200

    add default_documents

--- gdpr/setup/default_documents.py
+++ gdpr/setup/default_documents.py
@@ -9,6 +9,270 @@ def get_data():
         },
         {
             "doctype": "Personal Data Category",
-            "title": _("Birth Date")
+            "title": _("Payment terms")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Shipping terms")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Fax")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Tax ID")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Website")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Applicant Name")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Electronic identification data"),
+            "description": _("IP address, log-in data, cookies")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Information on family")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Fucntion grid")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Pictures/Images")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Video images"),
+            "description": _("From security cameras")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Criminal convictions/offences")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Personal health/medical info")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Sick days")
+        },    
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Leave/holidays")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Registered work hours")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Evaluation")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Training during employment")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Union membership")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("CV/résumé")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Workexperience"),
+            "description": _("Company/Address/Designation")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Education level/diplomas")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Credit card details")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Bank account")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Wage/salary")
+        },{
+            "doctype": "Personal Data Category",
+            "title": _("Religion")
+        },{
+            "doctype": "Personal Data Category",
+            "title": _("Insurances"),
+            "description": _("Health Insurance Provider")
+        },{
+            "doctype": "Personal Data Category",
+            "title": _("Marital status")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Work permit")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Sex")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Driver's license details"),
+            "description": _("Copy of Drivers license")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Social security number")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Copy of ID card")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Nationality")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("National ID card details")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Passport details"),
+            "description": _("Copy of passport")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Place of birth")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Languages")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Birthday/age")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Work phone number")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Personal phone number")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Personal e-mail address")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Name")
+        },
+        {
+            "doctype": "Personal Data Category",
+            "title": _("Creditworthiness")
+        },
+        {
+            "title": _("Customer"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Supplier"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Employee"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Visitor Website"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Newsletter-Subscribers"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Lead"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Job Applicant"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Opportunity"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Shareholders"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Temporary agency workers"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Contractors/consultants/freelancers"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Website end-users"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Tax consultant"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Bank"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Employee"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Customer"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Supplier"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Tax office"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Sales stuff"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Webhost"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Management"),
+            "doctype": "Data Subject Category"
+        },
+        {
+            "title": _("Sub-contracted processor"),
+            "doctype": "Data Subject Category"
         }
     ]
+
